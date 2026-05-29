319bba753a067a9cd25c64760a2d1f89d9f72979
Ollie Joestar <bigzzzid@gmail.com>
2026-05-29 12:00:12 +0200

    headlights lol

--- src/Car.tsx
+++ src/Car.tsx
@@ -1,5 +1,35 @@
+import { useRef, useLayoutEffect } from 'react'
 import * as THREE from 'three'
 import { COLOR_SCHEMES } from './options.ts'
+
+function Headlight({ position }: { position: [number, number, number] }) {
+  const lightRef = useRef<THREE.SpotLight>(null!)
+  const targetRef = useRef<THREE.Object3D>(null!)
+
+  useLayoutEffect(() => {
+    lightRef.current.target = targetRef.current
+  }, [])
+
+  return (
+    <>
+      <mesh position={position}>
+        <boxGeometry args={[0.18, 0.1, 0.05]} />
+        <meshLambertMaterial color={0xffffaa} emissive={0xffff66} emissiveIntensity={0.6} />
+      </mesh>
+      {/* <spotLight */}
+      {/*   ref={lightRef} */}
+      {/*   position={position} */}
+      {/*   angle={Math.PI / 6} */}
+      {/*   penumbra={0.2} */}
+      {/*   intensity={30} */}
+      {/*   distance={25} */}
+      {/*   color={COLOR_SCHEMES.default.directionalLight} */}
+      {/* /> */}
+      {/* Target sits 12 units forward in local car space — moves with the car */}
+      < object3D ref={targetRef} position={[position[0], position[1], position[2] + 12]} />
+    </>
+  )
+}
 
 export function Car({ carRef }: { carRef: React.RefObject<THREE.Mesh> }) {
   return (
@@ -15,13 +45,9 @@ export function Car({ carRef }: { carRef: React.RefObject<THREE.Mesh> }) {
           <meshLambertMaterial color={COLOR_SCHEMES.default.car} />
         </mesh>
 
-        {/* Headlights */}
-        {([-0.35, 0.35] as const).map((xOff) => (
-          <mesh key={xOff} position={[xOff, 0.05, 1.03]}>
-            <boxGeometry args={[0.18, 0.1, 0.05]} />
-            <meshLambertMaterial color={0xffffaa} emissive={0xffff66} emissiveIntensity={0.6} />
-          </mesh>
-        ))}
+        {/* Headlights with spotlights */}
+        <Headlight position={[-0.35, 0.05, 1.03]} />
+        <Headlight position={[0.35, 0.05, 1.03]} />
       </mesh>
     </>
   )
